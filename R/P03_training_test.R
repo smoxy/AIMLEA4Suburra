59e922b0c77f8ea347c374e90bc9c80edaf627ba
smoxy <smoxy@hotmail.it>
2023-06-02 02:04:34 +0000

    traininig after divison in train and test

--- R/P03_training_test.R
+++ R/P03_training_test.R
@@ -18,22 +18,60 @@ doParallel::registerDoParallel(cl)
 #registerDoSEQ()
 ################################################################################
 #####                  Random Forest with SMOTE Oversampling               #####
-set.seed(1234)
+set.seed(1234)  # For reproducibility
+Index <- caret::createDataPartition(dtm.With_stopW$character, p = .8, list = FALSE, times = 1)
+dtm.With_stopW_TRAIN            <- dtm.With_stopW[Index,]
+dtm.With_stopW_TEST             <- dtm.With_stopW[-Index,]
+Index <- caret::createDataPartition(dtm_collapsed.With_stopW$character, p = .8, list = FALSE, times = 1)
+dtm_collapsed.With_stopW_TRAIN  <- dtm_collapsed.With_stopW[Index,]
+dtm_collapsed.With_stopW_TEST   <- dtm_collapsed.With_stopW[-Index,]
+Index <- caret::createDataPartition(dtm_hybrid.With_stopW$character, p = .8, list = FALSE, times = 1)
+dtm_hybrid.With_stopW_TRAIN     <- dtm_hybrid.With_stopW[Index,]
+dtm_hybrid.With_stopW_TEST      <- dtm_hybrid.With_stopW[-Index,]
+Index <- caret::createDataPartition(dtm.No_stopW$character, p = .8, list = FALSE, times = 1)
+dtm.No_stopW_TRAIN              <- dtm.No_stopW[Index,]
+dtm.No_stopW_TEST               <- dtm.No_stopW[-Index,]
+Index <- caret::createDataPartition(dtm_collapsed.No_stopW$character, p = .8, list = FALSE, times = 1)
+dtm_collapsed.No_stopW_TRAIN    <- dtm_collapsed.No_stopW[Index,]
+dtm_collapsed.No_stopW_TEST     <- dtm_collapsed.No_stopW[-Index,]
+Index <- caret::createDataPartition(dtm_hybrid.No_stopW$character, p = .8, list = FALSE, times = 1)
+dtm_hybrid.No_stopW_TRAIN       <- dtm_hybrid.No_stopW[Index,]
+dtm_hybrid.No_stopW_TEST        <- dtm_hybrid.No_stopW[-Index,]
+
 # crea un oggetto di controllo per l'addestramento del modello.
 ctrl <- caret::trainControl(method = "repeatedcv", # metodo di validazione incrociata da utilizzare
-                     number = 3,         # il numero di fold nella validazione incrociata (10)
-                     repeats = 10,        # il numero di ripetizioni per la validazione incrociata (10)
-                     verboseIter = FALSE, # stampare o meno i messaggi iterativi durante l'addestramento
+                     number = 10,         # il numero di fold nella validazione incrociata (10)
+                     repeats = 5,        # il numero di ripetizioni per la validazione incrociata (10)
+                     verboseIter = TRUE, # stampare o meno i messaggi iterativi durante l'addestramento
                      sampling = "smote")  # metodo di campionamento da utilizzare ("smote")
 
-model_rf_smote <- caret::train(character ~ .,
-                               data = dtm.With_stopW,
+dtm_collapsed.With_stopW_model <- caret::train(character ~ .,
+                               data = dtm_collapsed.With_stopW_TRAIN,
                                method = "rf",
                                preProcess = c("scale", "center"),
                                trControl = ctrl,
                                allowParallel=TRUE)
 
-model_rf_smote <- randomForest.dtm.With_stopW
+dtm_collapsed.No_stopW_model <- caret::train(character ~ .,
+                               data = dtm_collapsed.No_stopW_TRAIN,
+                               method = "rf",
+                               preProcess = c("scale", "center"),
+                               trControl = ctrl,
+                               allowParallel=TRUE)
+
+dtm_hybrid.With_stopW_model <- caret::train(character ~ .,
+                               data = dtm_hybrid.With_stopW_TRAIN,
+                               method = "rf",
+                               preProcess = c("scale", "center"),
+                               trControl = ctrl,
+                               allowParallel=TRUE)
+
+dtm_hybrid.No_stopW_model <- caret::train(character ~ .,
+                               data = dtm_hybrid.No_stopW_TRAIN,
+                               method = "rf",
+                               preProcess = c("scale", "center"),
+                               trControl = ctrl,
+                               allowParallel=TRUE)
 
 
 df_smote <- SMOTE(X = df[ , colnames(dtm.With_stopW) != "character"],

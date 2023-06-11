bacba53f94801757597aad1a758f59b38e9ce3c0
smoxy <smoxy@hotmail.it>
2023-06-11 14:23:08 +0000

    update

--- R/P03_training_test.R
+++ R/P03_training_test.R
@@ -1,4 +1,4 @@
-install.packages("parallel",Ncpus = 4)
+#install.packages("parallel",Ncpus = 4)
 library(parallel)
 cores<-(as.numeric(parallel::detectCores()-2))
 installAndLoadPackages(c("caret", "smotefamily", "parallel", "doParallel", "bnclassify", "C50", "xtable"), cores = cores)
@@ -38,9 +38,9 @@ dtm_hybrid.With_stopW_TEST      <- dtm_hybrid.With_stopW$dtm_2[-dtm_hybrid.With_
 
 
 
-df.final['character'] <- as.factor(df.final['character'])
-df_collapsed.final['character'] <- as.factor(df_collapsed.final['character'])
-df_hybrid.final['character'] <- as.factor(df_hybrid.final['character'])
+#df.final['character'] <- as.factor(df.final['character'])
+#df_collapsed.final['character'] <- as.factor(df_collapsed.final['character'])
+#df_hybrid.final['character'] <- as.factor(df_hybrid.final['character'])
 Index <- caret::createDataPartition(df.final$character, p = .75, list = FALSE, times = 1)
 df.final_TRAIN           <- df.final[Index,]
 df.final_TEST            <- df.final[-Index,]
@@ -60,9 +60,31 @@ df_hybrid.final_TEST     <- df_hybrid.final[-Index,]
 # crea un oggetto di controllo per l'addestramento del modello
 ctrl <- caret::trainControl(method = "repeatedcv",
                             number = 10,
-                            repeats = 5,
+                            repeats = 3,
                             verboseIter = F)
 
+caretRF <- function(X_train, y_train, X_test, y_test, tuneControl){
+    tuneGrid <- expand.grid(mtry = seq(1, ncol(X_train), by = 1))
+    model <- caret::train(x = X_train,
+                          y = y_train,
+                          method = "rf",
+                          tuneGrid = tuneGrid,
+                          trControl = tuneControl,
+                          allowParallel=TRUE)
+
+    all_models_accuracy <- mean(model$resample$Accuracy)
+    prediction <- predict(model, newdata = X_test)
+    confusion_matrix <- caret::confusionMatrix(data = prediction, reference = y_test)
+    confusion_matrix <- round(confusion_matrix$byClass * 100, 2)  #"positive" "table" "overall" "byClass" "mode" "dots" 
+    latex_table <- xtable(as.data.frame(confusion_matrix), caption = "Metrics Table") # Sensitivity, Specificity, Precision
+    
+    return(list(
+        "model" = model,
+        "average.accuracy.across.models" = all_models_accuracy,
+        "prediction" = prediction,
+        "confusion_matrix" = confusion_matrix,
+        "latex_table" = latex_table))
+}
 
 ################################################################################
 #####                  Random Forest with SMOTE Oversampling               #####
@@ -139,8 +161,8 @@ tuneGrid <- expand.grid(trials = c(5, 10, 20, 30),
 
 ctrl <- caret::trainControl(method = "repeatedcv",
                             number = 10,
-                            repeats = 1,
-                            verboseIter = FALSE)
+                            repeats = 5,
+                            verboseIter = TRUE)
 
 caretC50 <- function(X_train, y_train, X_test, y_test, tuneControl, tuneGrid){
     model <- caret::train(x = X_train,
@@ -171,23 +193,22 @@ c50.collapsed.final <- caretC50(X_train = df_collapsed.final_TRAIN[, -c(which(na
 c50.hybrid.final    <- caretC50(X_train = df_hybrid.final_TRAIN[, -c(which(names(df_hybrid.final_TRAIN) == 'character'),which(names(df_hybrid.final_TRAIN) == 'script_line'))], y_train = df_hybrid.final_TRAIN$character, X_test=df_hybrid.final_TEST[, -c(which(names(df_hybrid.final_TEST) == 'character'),which(names(df_hybrid.final_TEST) == 'script_line'))], y_test=df_hybrid.final_TEST$character, tuneControl = ctrl, tuneGrid=tuneGrid)
 save(c50.final, c50.collapsed.final, c50.hybrid.final, file = "server_computation.DF_C50.RData")
 
-c50.dtm              <- caretC50(X_train = dtm_TRAIN, y_train = dtm$character[dtm.Index], X_test=dtm_TEST, y_test=dtm$character[-dtm.Index], tuneControl = ctrl, tuneGrid=NULL)
+c50.dtm              <- caretC50(X_train = dtm_TRAIN, y_train = dtm$character[dtm.Index], X_test=dtm_TEST, y_test=dtm$character[-dtm.Index], tuneControl = ctrl, tuneGrid)
 
-c50.dtm_collapsed    <- caretC50(dtm_collapsed_TRAIN, dtm_collapsed$character[dtm_collapsed.Index], dtm_collapsed_TEST, dtm_collapsed$character[-dtm_collapsed.Index], ctrl, tuneGrid=NULL)
+c50.dtm_collapsed    <- caretC50(dtm_collapsed_TRAIN, dtm_collapsed$character[dtm_collapsed.Index], dtm_collapsed_TEST, dtm_collapsed$character[-dtm_collapsed.Index], ctrl, tuneGrid)
 
-c50.dtm_hybrid       <- caretC50(dtm_hybrid_TRAIN, dtm_hybrid$character[dtm_hybrid.Index], dtm_hybrid_TEST, dtm_hybrid$character[-dtm_hybrid.Index], ctrl, tuneGrid=NULL)
+c50.dtm_hybrid       <- caretC50(dtm_hybrid_TRAIN, dtm_hybrid$character[dtm_hybrid.Index], dtm_hybrid_TEST, dtm_hybrid$character[-dtm_hybrid.Index], ctrl, tuneGrid)
+save(c50.dtm, c50.dtm_collapsed, c50.dtm_hybrid, file = "server_computation.DTM_C50.RData")
 
-c50.dtm.With_stopW            <- caretC50(dtm.With_stopW_TRAIN, dtm.With_stopW$character[dtm.With_stopW.Index], dtm.With_stopW_TEST, dtm.With_stopW$character[-dtm.With_stopW.Index], ctrl, tuneGrid=NULL)
+c50.dtm.With_stopW            <- caretC50(dtm.With_stopW_TRAIN, dtm.With_stopW$character[dtm.With_stopW.Index], dtm.With_stopW_TEST, dtm.With_stopW$character[-dtm.With_stopW.Index], ctrl, tuneGrid)
 
-c50.dtm_collapsed.With_stopW  <- caretC50(dtm_collapsed.With_stopW_TRAIN, dtm_collapsed.With_stopW$character[dtm_collapsed.With_stopW.Index], dtm_collapsed.With_stopW_TEST, dtm_collapsed.With_stopW$character[-dtm_collapsed.With_stopW.Index], ctrl, tuneGrid=NULL)
+c50.dtm_collapsed.With_stopW  <- caretC50(dtm_collapsed.With_stopW_TRAIN, dtm_collapsed.With_stopW$character[dtm_collapsed.With_stopW.Index], dtm_collapsed.With_stopW_TEST, dtm_collapsed.With_stopW$character[-dtm_collapsed.With_stopW.Index], ctrl, tuneGrid)
 
-c50.dtm_hybrid.With_stopW     <- caretC50(dtm_hybrid.With_stopW_TRAIN, dtm_hybrid.With_stopW$character[dtm_hybrid.With_stopW.Index], dtm_hybrid.With_stopW_TEST, dtm_hybrid.With_stopW$character[-dtm_hybrid.With_stopW.Index], ctrl, tuneGrid=NULL)
+c50.dtm_hybrid.With_stopW     <- caretC50(dtm_hybrid.With_stopW_TRAIN, dtm_hybrid.With_stopW$character[dtm_hybrid.With_stopW.Index], dtm_hybrid.With_stopW_TEST, dtm_hybrid.With_stopW$character[-dtm_hybrid.With_stopW.Index], ctrl, tuneGrid)
 
-c50.final           <- caretC50(X_train = df.final_TRAIN[, -c(which(names(df.final_TRAIN) == 'character'),which(names(df.final_TRAIN) == 'script_line'))], y_train = df.final_TRAIN$character, X_test=df.final_TEST[, -c(which(names(df.final_TEST) == 'character'),which(names(df.final_TEST) == 'script_line'))], y_test=df.final_TEST$character, tuneControl = ctrl, tuneGrid=tuneGrid)
+save(c50.dtm, c50.dtm_collapsed, c50.dtm_hybrid, c50.dtm.With_stopW, c50.dtm_collapsed.With_stopW, c50.dtm_hybrid.With_stopW, file = "server_computation.DTM_C50.RData")
 
-c50.collapsed.final <- caretC50(X_train = df_collapsed.final_TRAIN[, -c(which(names(df_collapsed.final_TRAIN) == 'character'),which(names(df_collapsed.final_TRAIN) == 'script_line'))], y_train = df_collapsed.final_TRAIN$character, X_test=df_collapsed.final_TEST[, -c(which(names(df_collapsed.final_TEST) == 'character'),which(names(df_collapsed.final_TEST) == 'script_line'))], y_test=df_collapsed.final_TEST$character, tuneControl = ctrl, tuneGrid=tuneGrid)
-
-c50.hybrid.final    <- caretC50(X_train = df_hybrid.final_TRAIN[, -c(which(names(df_hybrid.final_TRAIN) == 'character'),which(names(df_hybrid.final_TRAIN) == 'script_line'))], y_train = df_hybrid.final_TRAIN$character, X_test=df_hybrid.final_TEST[, -c(which(names(df_hybrid.final_TEST) == 'character'),which(names(df_hybrid.final_TEST) == 'script_line'))], y_test=df_hybrid.final_TEST$character, tuneControl = ctrl, tuneGrid=tuneGrid)
+rm(c50.final, c50.collapsed.final, c50.hybrid.final, c50.dtm, c50.dtm_collapsed, c50.dtm_hybrid, c50.dtm.With_stopW, c50.dtm_collapsed.With_stopW, c50.dtm_hybrid.With_stopW)
 
 #model accuracy
 #p.final <- predict(m, newdata = df.final_TEST[, -which(names(df.final_TEST) == "character")])
